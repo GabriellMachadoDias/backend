1b9265741e0fc3a806f1053c4ccc03febea49c8d
unknown <2414290114@ACADEMICO.IESB.BR>
2025-11-13 09:29:59 -0300

    Códigos da aula 12

--- aulas/aula12/app.js
+++ aulas/aula12/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const apidocsRouter = require('./routes/apidocs')
+const apidocsRouter = require('./routes/apidocs');
+const tarefasRouter = require('./routes/tarefaRouter');
 
 const app = express();
 
@@ -12,5 +13,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/api-docs', apidocsRouter);
+app.use('/tarefas', tarefasRouter);
 
 module.exports = app;
--- aulas/aula12/routes/apidocs.js
+++ aulas/aula12/routes/apidocs.js
@@ -2,17 +2,11 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yaml');
 const fs = require('fs');
-
-//Carrega o arquivo swagger
 const file = fs.readFileSync('./swagger.yaml', 'utf8');
-
-//Valida o arquivo swagger
-const sawaggerDoc = YAML.parse(file);
-
+const swaggerDoc = YAML.parse(file);
 const router = express.Router();
 
 router.use("/", swaggerUi.serve);
-
-router.get("/", swaggerUi.setup(sawaggerDoc));
+router.get("/", swaggerUi.setup(swaggerDoc));
 
 module.exports = router;
